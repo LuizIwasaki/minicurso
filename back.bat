509239101387e38bd61431b517de3c561be95e84
Luiz <70579914+LuizIwasaki@users.noreply.github.com>
2022-11-17 20:19:37 -0300

    Update back.bat

--- back.bat
+++ back.bat
@@ -1,13 +1,10 @@
 call npm init -y
-call npm i --save dotenv
 call npm i --save-dev typescript -D
 call npm i --save express
-call npm i --save jsonwebtoken
 call npm i --save typeorm
 call npm i --save @types/node
 call npm i --save http-status-codes
 call npm i --save mysql2
 call npm i --save-dev @types/express
 call npm i --save-dev ts-node-dev
-call npm i --save-dev @types/jsonwebtoken
 call tsc --init
